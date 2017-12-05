9161ff4b90aa405d6e741c13955ea100681b3967
abbas886 <abbas886@yahoo.com>
2017-12-05 14:50:32 +0530

    same comments - circle controller

--- src/main/java/com/stackroute/activitystream/UserController.java
+++ src/main/java/com/stackroute/activitystream/UserController.java
@@ -17,6 +17,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.stackroute.activitystream.dao.UserDAO;
 import com.stackroute.activitystream.model.User;
 
+//see similar comments in circle controller.
 @RestController
 public class UserController {
 	
